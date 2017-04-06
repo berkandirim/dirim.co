a6f30c1eed1b7b5e0e24633d590c2f2b4f57d401
berkandirim <berkandirim@gmail.com>
2017-04-06 16:53:07 +0300

    background-color

--- source/scripts/app.js
+++ source/scripts/app.js
@@ -3,5 +3,8 @@
  */
 
 (function(window, document) {
-    
+    const page = document.querySelector('.page-wrapper');
+    document.addEventListener('DOMContentLoaded', function(e) {
+        page.style.display = 'block';
+    });
 })(window, document);
