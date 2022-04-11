4bcd84fa43959d2ce059249013a7ae2938cea203
Berkan Dirim <berkandirim@gmail.com>
2022-04-11 22:41:33 +0200

    content update

--- components/Links.vue
+++ components/Links.vue
@@ -2,15 +2,15 @@
   <footer class="contact section">
     <h2>Contact</h2>
     <ul class="contact-links">
-      <li v-for="link in links"
-          :key="link.name">
+      <li v-for="link in links" :key="link.name">
         <a
           class="link"
           :title="link.name"
           :href="link.link"
           target="_blank"
           rel="noreferrer"
-        >{{link.name}}</a>
+          >{{ link.name }}</a
+        >
       </li>
     </ul>
   </footer>
@@ -18,54 +18,53 @@
 
 <script>
 export default {
-  data () {
+  data() {
     return {
       links: [
         {
-          name: 'Twitter',
-          link: 'https://www.twitter.com/berkandirim'
-        },
-        {
           name: 'LinkedIn',
-          link: 'https://tr.linkedin.com/in/berkandirim'
+          link: 'https://tr.linkedin.com/in/berkandirim',
         },
         {
           name: 'Github',
-          link: 'https://www.github.com/berkandirim'
+          link: 'https://www.github.com/berkandirim',
         },
         {
           name: 'Email',
-          link: 'mailto:berkandirim@gmail.com'
-        }
-      ]
+          link: 'mailto:berkandirim@gmail.com',
+        },
+        {
+          name: 'Twitter',
+          link: 'https://www.twitter.com/berkandirim',
+        },
+      ],
     }
-  }
+  },
 }
 </script>
 
 <style lang="scss">
-  @import "../assets/scss/vars";
+@import '../assets/scss/vars';
 
-  .contact {
-    &-links {
-      margin-left: -4px;
-      overflow-x: auto;
-      li {
-        @media screen and (min-width: 721px) {
-          display: inline-block;
-          &+li:before {
-            content: '/';
-            margin: 0 10px;
-          }
+.contact {
+  &-links {
+    margin-left: -4px;
+    overflow-x: auto;
+    li {
+      @media screen and (min-width: 721px) {
+        display: inline-block;
+        & + li:before {
+          content: '/';
+          margin: 0 10px;
         }
       }
-      a {
-        margin: 6px 0;
-        @media screen and (min-width: 721px) {
-          margin: 0;
-        }
-
+    }
+    a {
+      margin: 6px 0;
+      @media screen and (min-width: 721px) {
+        margin: 0;
       }
     }
   }
+}
 </style>
